9dba899f0099d98a787e60af92a03f185f844c15
Alex <alex@MacBook-Air-Alex.local>
2026-05-09 22:00:49 +0300

    Remove obsolete awg_api.py shim

--- awg_api.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-# Backward-compatibility shim.
-# Canonical API implementation lives in api_core.py.
-from api_core import AWGManagerAPIHandler, start_api_server, main
-
-
-if __name__ == '__main__':
-    main()
